ff765bc528457de59f1f4cfef6c4e6f0d42f9ba9
Visal Raza Zaidi <visalraza@gmail.com>
2026-08-26 19:39:26 +0500

    Updated the Resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1kjubgaifC93rUYGyRQzwPw8Bq15VKV-L/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1YllG6Ag2unnCu3JAAQ8-PFIA-S9AGrDS/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
